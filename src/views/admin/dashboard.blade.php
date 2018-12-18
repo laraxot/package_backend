5dc0862cd8087f07350a8e3558cddbbd88c31eed
marco76tv <marco.sottana@gmail.com>
2018-12-18 20:46:34 +0100

    ulteriore messaggio di debug

--- src/views/admin/dashboard.blade.php
+++ src/views/admin/dashboard.blade.php
@@ -1,5 +1,6 @@
 @if(View::exists('adm_theme::admin.dashboard'))
 @include('adm_theme::admin.dashboard')
 @else
-   <h3>'adm_theme::admin.dashboard' not exists </h3>
+   <h3>'adm_theme::admin.dashboard' not exists [dash]</h3>
+   {{ Theme::asset('adm_theme::admin.dashboard.blade.php') }}
 @endif
