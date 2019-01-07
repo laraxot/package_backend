b6b01f00c4b06fe507c8f4f19ed64340365fe794
marco76tv <marco.sottana@gmail.com>
2019-01-07 21:40:18 +0100

    .

--- src/Controllers/ArtisanController.php
+++ src/Controllers/ArtisanController.php
@@ -8,16 +8,16 @@ use Artisan;
 
 class ArtisanController extends Controller
 {
-    public function exe($comando)
+    public function exe($command, array $arguments = [])
     {
         try {
             $output = '';
             
-            Artisan::call($comando);
+            Artisan::call($command,$arguments);
             $output .= '[<pre>' . Artisan::output() . '</pre>]';
             return $output;  // dato che mi carico solo le route minime menufull.delete non esiste.. impostare delle route comuni.
         } catch (Exception $e) {
-            return '<br/>'.$comando.' non effettuato';
+            return '<br/>'.$command.' non effettuato';
         }
         //return view('Backend::admin.index')->with('output', $output);
     }
