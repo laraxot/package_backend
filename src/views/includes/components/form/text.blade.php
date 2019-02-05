5f2a918ea991cf570b5ef3a2c4fd646691c048c2
marco76tv <marco.sottana@gmail.com>
2019-02-05 14:10:05 +0100

    form component algolia places , da valutare sostituzione di google per i costi

--- src/views/includes/components/form/text.blade.php
+++ src/views/includes/components/form/text.blade.php
@@ -1,12 +1,9 @@
 @php
-	if(isset($attributes['label']))
-		$label=$attributes['label'];
-	else
-		$label=trans($lang.'.'.$name);
+	$label=isset($attributes['label'])?$attributes['label']:trans($lang.'.'.$name);
 @endphp
 <div class="form-group{{ $errors->has($name) ? ' has-error' : '' }}">
 	{{ Form::label($name, $label , ['class' => 'col-md-4 control-label']) }}
-	<div class="col-md-6">	
+	<div class="col-md-6">
 		{{ Form::text($name, $value, array_merge(['class' => 'form-control'], $attributes)) }}
 		@if ( $errors->has($name) )
 			<span class="help-block">
--- src/views/includes/components/form/textarea.blade.php
+++ src/views/includes/components/form/textarea.blade.php
@@ -1,11 +1,10 @@
 @php
-	if(isset($attributes['label']))
-		$label=$attributes['label'];
-	else
-		$label=trans($lang.'.'.$name);
+	$label=isset($attributes['label'])?$attributes['label']:trans($lang.'.'.$name);
 @endphp
 <div class="form-group{{ $errors->has($name) ? ' has-error' : '' }}">
-	{{ Form::label($name,  $label, ['class' => 'col-md-4 control-label']) }}
+	<div class="col-md-4">
+		{{ Form::label($name,  $label, ['class' => 'control-label']) }}
+	</div>
 	<div class="col-md-6">	
 		{{ Form::textarea($name, $value, array_merge(['class' => 'form-control'], $attributes)) }}
 		@if ( $errors->has($name) )
